224b58f9a28200ac941acf440c85996cd6a7316f
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2023-02-16 14:23:00 +0900

    first commit

---
+++ utils/atari_wrappers.py
@@ -0,0 +1,182 @@
+# ref -> https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
+
+from collections import deque
+import cv2
+import gym
+import numpy as np
+
+class NoopResetEnv(gym.Wrapper):
+    def __init__(self, env, noop_max=30):
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.noop_action = 0
+        assert env.unwrapped.get_action_meanings()[0] == "NOOP"
+
+    def reset(self, **kwargs):
+        self.env.reset(**kwargs)
+        noops = self.env.unwrapped.np_random.randint(1, self.noop_max+1)
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, action):
+        return self.env.step(action)
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env, skip=4):
+        gym.Wrapper.__init__(self, env)
+        self._obs_buffer = np.zeros((2,)+env.observation_space.shape,
+                                    dtype=np.uint8)
+        self._skip       = skip
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+    def step(self, action):
+        total_reward = 0.0
+        done = None
+        for i in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            if i == self._skip - 2:
+                self._obs_buffer[0] = obs
+            if i == self._skip - 1:
+                self._obs_buffer[1] = obs
+            total_reward += reward
+            if done:
+                break
+        max_frame = self._obs_buffer.max(axis=0)
+        return max_frame, total_reward, done, info
+
+class EpisodicLifeEnv(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+        self.lives = 0
+        self.was_real_done  = True
+
+    def reset(self, **kwargs):
+        if self.was_real_done:
+            obs = self.env.reset(**kwargs)
+        else:
+            obs, _, _, _ = self.env.step(0)
+        self.lives = self.env.unwrapped.ale.lives()
+        return obs
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.was_real_done = done
+        lives = self.env.unwrapped.ale.lives()
+        if lives < self.lives and lives > 0:
+            done = True
+        self.lives = lives
+        return obs, reward, done, info
+
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+        self.fire_action = 1
+        assert env.unwrapped.get_action_meanings()[1] == "FIRE"
+
+    def reset(self, **kwargs):
+        self.env.reset(**kwargs)
+        obs, _, done, _ = self.env.step(self.fire_action)
+        if done:
+            self.env.reset(**kwargs)
+        return obs
+
+    def step(self, action):
+        return self.env.step(action)
+
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env, width=224, height=224,
+                grayscale=True):
+        super().__init__(env)
+        self._width = width
+        self._height = height
+        self._grayscale = grayscale
+        if self._grayscale:
+            num_colors = 1
+        else:
+            num_colors = 3
+        new_space = gym.spaces.Box(
+            low=0,
+            high=255,
+            shape=(self._height, self._width, num_colors),
+            dtype=np.uint8,
+        )
+        self.observation_space = new_space
+
+    def observation(self, obs):
+        if self._grayscale:
+            obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY)
+        obs = cv2.resize(
+            obs, (self._width, self._height),
+            interpolation=cv2.INTER_AREA
+        )
+        if self._grayscale:
+            obs = np.expand_dims(obs, -1)
+        return obs
+
+class ClipRewardEnv(gym.RewardWrapper):
+    def __init__(self, env):
+        gym.RewardWrapper.__init__(self, env)
+
+    def reward(self, reward):
+        return np.sign(reward)
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k):
+        gym.Wrapper.__init__(self, env)
+        self.k = k
+        self.frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0, high=255,
+                                            shape=((k,) + shp),
+                                            dtype=env.observation_space.dtype)
+
+    def reset(self):
+        ob = self.env.reset()
+        for _ in range(self.k):
+            self.frames.append(ob)
+        return self._get_ob()
+
+    def step(self, action):
+        ob, reward, done, info = self.env.step(action)
+        self.frames.append(ob)
+        return self._get_ob(), reward, done, info
+
+    def _get_ob(self):
+        assert len(self.frames) == self.k
+        return LazyFrames(list(self.frames))
+
+class LazyFrames(object):
+    def __init__(self, frames):
+        self._frames = frames
+        self._out = None
+
+    def _force(self):
+        if self._out is None:
+            self._out = np.stack(self._frames)
+            self._frames = None
+        return self._out
+
+    def __array__(self, dtype=None):
+        out = self._force()
+        if dtype is not None:
+            out = out.astype(dtype)
+        return out
+
+    def __len__(self):
+        return len(self._force())
+
+    def __getitem__(self, i):
+        return self._force()[i]
+
+    def count(self):
+        frames = self._force()
+        return frames.shape[frames.ndim - 1]
+
+    def frame(self, i):
+        return self._force()[..., i]
---
+++ utils/pong_env.py
@@ -0,0 +1,51 @@
+from atari_wrappers import *
+import argparse
+import cv2
+import gym
+import numpy as np
+import pathlib
+root_path = pathlib.Path(".")
+parser = argparse.ArgumentParser()
+parser.add_argument("--video_name", type=str)
+args = parser.parse_args()
+
+def make_pong_env(noop_max=30, skip=4, width=224, height=224):
+    env = gym.make('PongNoFrameskip-v4')
+    env = NoopResetEnv(env, noop_max=noop_max)
+    env = MaxAndSkipEnv(env, skip=skip)
+    env = EpisodicLifeEnv(env)
+    env = FireResetEnv(env)
+    env = WarpFrame(env, width=width, height=height, grayscale=False)
+    env = ClipRewardEnv(env)
+    env = FrameStack(env, skip)
+    return env
+
+def make_pong_video(env, video_path, width=224, height=224, reward_font_size=20, model=None):
+    obs = env.reset()
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    video = cv2.VideoWriter(str(video_path), fourcc, 15.0, (width, height))
+    total_reward = 0
+    done = False
+    while not done:
+        #for i in range(len(obs)):
+        #    frame = obs[i]
+        frame = obs[-1]
+        frame = cv2.putText(frame, f"Reward:{int(total_reward)}", (0, height-reward_font_size),
+                            cv2.FONT_HERSHEY_PLAIN, 1.5, (255,255,0), 1, cv2.LINE_AA)
+        video.write(frame)
+        if model is not None:
+            action = model(obs)
+        else:
+            action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+    video.release()
+    return
+
+if __name__=="__main__":
+    video_name = args.video_name
+    videos_path = root_path / "videos"
+    video_path = videos_path / video_name
+    env = make_pong_env()
+    make_pong_video(env, video_path)
+
